b951c8fc2cd57523e318394e60ac8c7ba7c75444
KavinSelvaraj <kavinselvaraj@Kavins-Mac.local>
2020-05-26 10:42:03 +0530

    mad Lab Generator

---
+++ madLab_genrator.py
@@ -0,0 +1,16 @@
+# Mad Libs on python
+
+
+# Input From the user
+
+color = input("Enter an color: ")
+celebrity = input("Enter a Celebrity: ")
+nonu = input("Enter a pural noun: ")
+
+# mini mad lab
+
+print(f"roses are {color}")
+print(f"{nonu} are blue")
+print(f"I love {celebrity}")
+
+
--- tkinter.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-window = tk.TK()
-
-window.title("GUI")
-
-lable = tk.lable(window, text="Helloworld").pack()
-
-window.mainloop()
