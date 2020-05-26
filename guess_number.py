992ad56cc797d6ea248a81c379337ed5b19dbbab
KavinSelvaraj <kavinselvaraj@Kavins-Mac.local>
2020-05-26 17:54:15 +0530

    random Number

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,20 @@
 # Import random module
-
 import random
 
+# Random number Variable
 randomNumber = random.randrange(1, 50)
 
+# Input From the User
 userNumber = int(input("Enter Random number between 1 to 50: "))
+
+# Condition for random NUmber Guess Execution
 while userNumber != randomNumber:
+
     if randomNumber > userNumber:
         print("Enter higher value number: ")
         userNumber = int(input("Enter Random number between 1 to 50: "))
     else:
         print("Enter Lower value number: ")
         userNumber = int(input("Enter Random number between 1 to 50: "))
+
 print(f"Your guess is Correct {userNumber}:", randomNumber)
